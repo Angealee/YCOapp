bd28f400c79792bbf276fccc8b346c4b102e9bf9
Angeale <koby.macale24@gmail.com>
2026-02-22 12:00:10 +0800

    fixed some dependencies in local Angeale

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.devfaculty.ycoapp',
   appName: 'YCOapp',
-  webDir: 'build'
+  webDir: 'dist'
 };
 
 export default config;
